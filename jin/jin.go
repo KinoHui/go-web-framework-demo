79e3ca4c48e4f98cc70948f86426cc5d30386135
jyh-centos9 <568000273@qq.com>
2024-11-05 16:25:22 +0800

    finish templates

--- jin/jin.go
+++ jin/jin.go
@@ -1,8 +1,11 @@
 package jin
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path"
+	"strings"
 )
 
 // HandlerFunc defines the request handler used by gee
@@ -10,8 +13,10 @@ type HandlerFunc func(*Context)
 
 type Engine struct {
 	*RouterGroup
-	router *router
-	groups []*RouterGroup // store all groups
+	router        *router
+	groups        []*RouterGroup     // store all groups
+	htmlTemplates *template.Template // for html render
+	funcMap       template.FuncMap   // for html render
 }
 
 type RouterGroup struct {
@@ -42,6 +47,11 @@ func (group *RouterGroup) Group(prefix string) *RouterGroup {
 	return newGroup
 }
 
+// Use is defined to add middleware to the group
+func (group *RouterGroup) Use(middlewares ...HandlerFunc) {
+	group.middlewares = append(group.middlewares, middlewares...)
+}
+
 func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
 	pattern := group.prefix + comp
 	log.Printf("Route %4s - %s", method, pattern)
@@ -58,11 +68,51 @@ func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
 	group.addRoute("POST", pattern, handler)
 }
 
+// create static handler
+func (group *RouterGroup) createStaticHandler(relativePath string, fs http.FileSystem) HandlerFunc {
+	absolutePath := path.Join(group.prefix, relativePath)
+	fileServer := http.StripPrefix(absolutePath, http.FileServer(fs))
+	return func(c *Context) {
+		file := c.Param("filepath")
+		// Check if file exists and/or if we have permission to access it
+		if _, err := fs.Open(file); err != nil {
+			c.Status(http.StatusNotFound)
+			return
+		}
+
+		fileServer.ServeHTTP(c.Writer, c.Req)
+	}
+}
+
+// serve static files
+func (group *RouterGroup) Static(relativePath string, root string) {
+	handler := group.createStaticHandler(relativePath, http.Dir(root))
+	urlPattern := path.Join(relativePath, "/*filepath")
+	// Register GET handlers
+	group.GET(urlPattern, handler)
+}
+
 func (e *Engine) Run(port string) error {
 	return http.ListenAndServe(port, e)
 }
 
-func (e *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	var middlewares []HandlerFunc
+	for _, group := range engine.groups {
+		if strings.HasPrefix(req.URL.Path, group.prefix) {
+			middlewares = append(middlewares, group.middlewares...)
+		}
+	}
 	c := newContext(w, req)
-	e.router.handle(c)
+	c.handlers = middlewares
+	c.engine = engine
+	engine.router.handle(c)
+}
+
+func (engine *Engine) SetFuncMap(funcMap template.FuncMap) {
+	engine.funcMap = funcMap
+}
+
+func (engine *Engine) LoadHTMLGlob(pattern string) {
+	engine.htmlTemplates = template.Must(template.New("").Funcs(engine.funcMap).ParseGlob(pattern))
 }
--- main.go
+++ main.go
@@ -1,40 +1,50 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"net/http"
+	"time"
 	"web-frame-demo/jin"
 )
 
+type student struct {
+	Name string
+	Age  int8
+}
+
+func FormatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
+}
+
 func main() {
 	r := jin.New()
-	r.GET("/index", func(c *jin.Context) {
-		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
+	r.Use(jin.Logger())
+	r.SetFuncMap(template.FuncMap{
+		"FormatAsDate": FormatAsDate,
 	})
-	v1 := r.Group("/v1")
-	{
-		v1.GET("/", func(c *jin.Context) {
-			c.HTML(http.StatusOK, "<h1>Hello jin</h1>")
-		})
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/assets", "./static")
 
-		v1.GET("/hello", func(c *jin.Context) {
-			// expect /hello?name=jinktutu
-			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	stu1 := &student{Name: "jinktutu", Age: 20}
+	stu2 := &student{Name: "Jack", Age: 22}
+	r.GET("/", func(c *jin.Context) {
+		c.HTML(http.StatusOK, "css.tmpl", nil)
+	})
+	r.GET("/students", func(c *jin.Context) {
+		c.HTML(http.StatusOK, "arr.tmpl", jin.H{
+			"title":  "jin",
+			"stuArr": [2]*student{stu1, stu2},
 		})
-	}
-	v2 := r.Group("/v2")
-	{
-		v2.GET("/hello/:name", func(c *jin.Context) {
-			// expect /hello/jinktutu
-			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-		})
-		v2.POST("/login", func(c *jin.Context) {
-			c.JSON(http.StatusOK, jin.H{
-				"username": c.PostForm("username"),
-				"password": c.PostForm("password"),
-			})
-		})
+	})
 
-	}
+	r.GET("/date", func(c *jin.Context) {
+		c.HTML(http.StatusOK, "custom_func.tmpl", jin.H{
+			"title": "jin",
+			"now":   time.Date(2019, 8, 17, 0, 0, 0, 0, time.UTC),
+		})
+	})
 
 	r.Run(":9999")
 }
